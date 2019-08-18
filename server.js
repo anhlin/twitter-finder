bb899d7ef6d6761c5c7a296a2541b44886701a4f
anhlin <anhlin@ucsc.edu>
2019-08-18 16:07:22 -0700

    heroku

--- server.js
+++ server.js
@@ -34,13 +34,9 @@ app.get("/user", (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
-  app.get(
-    "*",
-    (req,
-    res => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    })
-  );
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.listen(port, function() {
