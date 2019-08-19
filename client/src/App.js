be92fad8717d8776487e55f06def15b594fb7930
anhlin <anhlin@ucsc.edu>
2019-08-19 00:56:28 -0700

    styling/refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -1,93 +1,100 @@
-import "./App.css";
-import React, { useState, Fragment } from "react";
-import Navbar from "./components/layout/Navbar";
-import axios from "axios";
-import Users from "./components/users/Users";
-import UserInfo from "./components/users/UserInfo";
-import Search from "./components/users/Search";
-import Alert from "./components/layout/alert";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import './App.css';
+import React, { useState, Fragment } from 'react';
+import Navbar from './components/layout/Navbar';
+import axios from 'axios';
+import Users from './components/users/Users';
+import UserInfo from './components/users/UserInfo';
+import Search from './components/users/Search';
+import Alert from './components/layout/alert';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoad] = useState(false);
-  const [alert, setAlert] = useState(null);
-  const [userInfo, setUserInfo] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [loading, setLoad] = useState(false);
+    const [alert, setAlert] = useState(null);
+    const [userInfo, setUserInfo] = useState([]);
 
-  const clear = () => {
-    //this.setState({ users: [], loading: false });
-    setUsers([]);
-    setLoad(false);
-  };
-
-  const searchUsers = text => {
-    setLoad(true);
-    axios
-      .get("/api", {
-        params: { q: text }
-      })
-      .then(res => {
-        setUsers(res.data);
+    const clear = () => {
+        //this.setState({ users: [], loading: false });
+        setUsers([]);
         setLoad(false);
-        setAlert(null);
-        //this.setState({ users: res.data, loading: false, alert: null });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
+    };
 
-  const getUser = screen_name => {
-    axios
-      .get("/api/user", {
-        params: { screen_name: screen_name }
-      })
-      .then(res => {
-        setUserInfo(res.data);
-        //this.setState({ userInfo: res.data });
-      });
-  };
+    const searchUsers = text => {
+        setLoad(true);
+        axios
+            .get('/api', {
+                params: { q: text }
+            })
+            .then(res => {
+                setUsers(res.data);
+                setLoad(false);
+                setAlert(null);
+                //this.setState({ users: res.data, loading: false, alert: null });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    };
 
-  const changeAlert = (msg, type) => {
-    setAlert({ msg: msg, type: type });
-    //this.setState({ alert: { msg: msg, type: type } });
-  };
+    const getUser = screen_name => {
+        axios
+            .get('/api/user', {
+                params: { screen_name: screen_name }
+            })
+            .then(res => {
+                setUserInfo(res.data);
+                //this.setState({ userInfo: res.data });
+            });
+    };
 
-  return (
-    <Router>
-      <div className="App">
-        <Navbar title="Twitter Finder" icon="fab fa-twitter" />
-        <div className="container">
-          <Alert alert={alert} />
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={props => (
-                <Fragment>
-                  <Search
-                    searchUsers={searchUsers}
-                    clear={clear}
-                    showClear={users.length > 0 ? true : false}
-                    setAlert={changeAlert}
-                  />
-                  <Users loading={loading} users={users} />
-                </Fragment>
-              )}
-            />
-            <Route
-              exact
-              path="/user/:screen_name"
-              render={props => (
-                <UserInfo {...props} getUser={getUser} userInfo={userInfo} />
-              )}
-            />
-            } />
-          </Switch>
-        </div>
-      </div>
-    </Router>
-  );
+    const changeAlert = (msg, type) => {
+        setAlert({ msg: msg, type: type });
+        setTimeout(() => setAlert(null), 3000);
+        //this.setState({ alert: { msg: msg, type: type } });
+    };
+
+    return (
+        <Router>
+            <div className="App">
+                <Navbar title="Twitter Finder" icon="fab fa-twitter" />
+                <div className="container">
+                    <Alert alert={alert} />
+                    <Switch>
+                        <Route
+                            exact
+                            path="/"
+                            render={props => (
+                                <Fragment>
+                                    <Search
+                                        searchUsers={searchUsers}
+                                        clear={clear}
+                                        showClear={
+                                            users.length > 0 ? true : false
+                                        }
+                                        setAlert={changeAlert}
+                                    />
+                                    <Users loading={loading} users={users} />
+                                </Fragment>
+                            )}
+                        />
+                        <Route
+                            exact
+                            path="/user/:screen_name"
+                            render={props => (
+                                <UserInfo
+                                    {...props}
+                                    getUser={getUser}
+                                    userInfo={userInfo}
+                                />
+                            )}
+                        />
+                        } />
+                    </Switch>
+                </div>
+            </div>
+        </Router>
+    );
 };
 
 export default App;
--- client/src/components/users/Search.js
+++ client/src/components/users/Search.js
@@ -1,50 +1,50 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 const Search = ({ searchUsers, showClear, clear, setAlert }) => {
-  const [text, setText] = useState("");
+    const [text, setText] = useState('');
 
-  const onChange = event => setText(event.target.value);
+    const onChange = event => setText(event.target.value);
 
-  const onSubmit = event => {
-    event.preventDefault();
-    if (text.trim().length !== 0) {
-      searchUsers(text);
-      setText("");
-    } else {
-      setAlert("Enter a Name!", "light");
-    }
-  };
+    const onSubmit = event => {
+        event.preventDefault();
+        if (text.trim().length !== 0) {
+            searchUsers(text);
+            setText('');
+        } else {
+            setAlert('Enter a Name!', 'light');
+        }
+    };
 
-  return (
-    <div>
-      <form onSubmit={onSubmit} className="form">
-        <input
-          type="text"
-          name="text"
-          placeholder="Enter a Name"
-          value={text}
-          onChange={onChange}
-        />
-        <input
-          type="submit"
-          value="Search"
-          className="btn btn-dark btn-block"
-        />
-      </form>
-      {showClear && (
-        <button className="btn btn-light btn-block" onClick={clear}>
-          Clear
-        </button>
-      )}
-    </div>
-  );
+    return (
+        <div>
+            <form onSubmit={onSubmit} className="form">
+                <input
+                    type="text"
+                    name="text"
+                    placeholder="Enter a Name (e.g. 'Bill')"
+                    value={text}
+                    onChange={onChange}
+                />
+                <input
+                    type="submit"
+                    value="Search"
+                    className="btn btn-dark btn-block"
+                />
+                {showClear && (
+                    <button className="btn btn-light btn-block" onClick={clear}>
+                        Clear Results
+                    </button>
+                )}
+            </form>
+        </div>
+    );
 };
 Search.propTypes = {
-  searchUsers: PropTypes.func.isRequired,
-  clear: PropTypes.func.isRequired,
-  showClear: PropTypes.bool.isRequired,
-  setAlert: PropTypes.func.isRequired
+    searchUsers: PropTypes.func.isRequired,
+    clear: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired
 };
 
 export default Search;
