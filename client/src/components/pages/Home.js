a075d2d6b14cc928ba53819a02f56da37f6d85e0
anhlin <anhlin@ucsc.edu>
2019-09-05 00:28:48 -0700

    fixing buttons

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -4,13 +4,15 @@ import { Link } from 'react-router-dom';
 const Home = () => {
     return (
         <div>
-            <div className="button-wrapper ">
-                <Link to="/search">
-                    <button className="button">Search</button>
-                </Link>
-                <Link to="/random">
-                    <button className="button">Random</button>
-                </Link>
+            <div className="button-wrapper">
+                <div className="d-flex justify-content-center">
+                    <Link to="/search">
+                        <button className="button">Search</button>
+                    </Link>
+                    <Link to="/random">
+                        <button className="button">Random</button>
+                    </Link>
+                </div>
             </div>
             <p
                 style={{
